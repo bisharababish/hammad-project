ee7491c48f7b33302e6b6863baa818e71f0c071c
bisharababish <62569803+bisharababish@users.noreply.github.com>
2026-06-06 17:21:05 +0300

    switch to node-server, remove cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -8,7 +7,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     tanstackStart(),
-    nitro(),
     viteReact(),
     tailwindcss(),
     tsconfigPaths(),
